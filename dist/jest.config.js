cf9a538b86ab295cf2a532f21a7ffcc97f2badc4
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-09-27 12:08:41 +0600

    chore: Add dist to .gitignore

--- dist/jest.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ts_jest_1 = require("ts-jest");
-const config = Object.assign(Object.assign({}, (0, ts_jest_1.createDefaultPreset)()), { testMatch: ["**/*.test.ts"] });
-exports.default = config;
--- dist/src/utils/generateRandomCode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateVerificationCode = void 0;
-const generateVerificationCode = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-};
-exports.generateVerificationCode = generateVerificationCode;
-exports.default = exports.generateVerificationCode;
